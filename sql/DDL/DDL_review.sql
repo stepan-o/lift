aa708079619b7e8f0cb1c383c7f3ae82498759ca
Stepan Oskin <stepanoskin007@gmail.com>
2019-11-06 19:08:48 -0500

    DDL for Yelp database

---
+++ sql/DDL/DDL_review.sql
@@ -0,0 +1,20 @@
+drop table if exists review;
+-- auto-generated definition
+create table review
+(
+    stars       numeric,
+    review_id   text not null
+        constraint review_pk
+            primary key,
+    user_id     text,
+    funny       integer,
+    text        text,
+    date        text,
+    useful      integer,
+    cool        integer,
+    business_id text
+);
+
+alter table review
+    owner to yelp;
+
---
+++ sql/DDL/DDL_yelp_user.sql
@@ -0,0 +1,33 @@
+drop table if exists yelp_user;
+-- auto-generated definition
+create table yelp_user
+(
+    fans               integer,
+    compliment_cute    integer,
+    useful             integer,
+    compliment_cool    integer,
+    yelping_since      text,
+    compliment_funny   integer,
+    review_count       integer,
+    average_stars      numeric,
+    compliment_more    integer,
+    funny              integer,
+    compliment_list    integer,
+    compliment_writer  integer,
+    friends            text,
+    compliment_hot     integer,
+    compliment_plain   integer,
+    name               text,
+    elite              text,
+    compliment_photos  integer,
+    compliment_profile integer,
+    compliment_note    integer,
+    user_id            text not null
+        constraint yelp_user_pk
+            primary key,
+    cool               integer
+);
+
+alter table yelp_user
+    owner to yelp;
+
