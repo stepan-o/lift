8e339a60806c4c0beabaf1ad12e3c0ce3a960b6d
Stepan Oskin <stepanoskin007@gmail.com>
2019-11-10 15:27:08 -0500

    finish questions 2-4

--- sql/queries/lift_challenge.sql
+++ sql/queries/lift_challenge.sql
@@ -2,19 +2,14 @@
 
 /* Lift challenge questions */
 
--- Question 1 is related to data ingestion
+-- Question 1 is related to data ingestion, see sql/DDL and /DML folders
 
 -- Question 2: Top 10 restaurants in Toronto with the highest popularity
 -- option 1: most popular = most reviews and stars > 4.4
-SELECT
-    name,
-    city,
-    categories,
-    review_count,
-    stars
+SELECT name, review_count, stars
 FROM business
 WHERE
-        city='Toronto'
+      city='Toronto'
   AND
         categories LIKE '%Restaurants%'
   AND
@@ -23,12 +18,7 @@ ORDER BY review_count DESC, stars DESC
 LIMIT 10;
 
 -- option 2: most popular = highest rated, ordered by review count
-SELECT
-    name,
-    city,
-    categories,
-    review_count,
-    stars
+SELECT name, review_count, stars
 FROM business
 WHERE
         city='Toronto'
@@ -39,39 +29,39 @@ LIMIT 10;
 
 
 -- Question 3: How many Canadian residents reviewed the business “Mon Ami Gabi” in last 1 year
----- ********** TODO UNFINISHED
 SELECT
-    u.user_id,
-    u.name,
-    COALESCE(rc.rev_can, 0) AS reviews_can,
-    u.review_count,
-    ROUND(CAST(COALESCE(rc.rev_can, 0) AS DECIMAL) / u.review_count, 2) AS can_perc
-FROM yelp_user AS u
-         LEFT JOIN (
-    SELECT
-        u.user_id AS user_id,
-        COUNT(*) AS rev_can
-    FROM yelp_user AS u
-             JOIN review_cleaned r
-                  ON u.user_id = r.user_id
-             JOIN business b
-                  ON r.business_id = b.business_id
-    WHERE b.canadian=TRUE
-    GROUP BY u.user_id
-    HAVING COUNT(*) NOTNULL
-) AS rc
-
-                   ON u.user_id = rc.user_id
-WHERE u.review_count != 0
-ORDER BY can_perc DESC;
+    COUNT(DISTINCT can_users.can_user_id)
+FROM (
+         SELECT
+             r.user_id can_user_id,
+             -- compute the ratio of reviews in Canadian provinces vs total reviews
+             COALESCE(SUM(CASE WHEN b.state IN
+                                    ('NL', 'PE', 'NS', 'NB', 'QC', 'ON', 'MB', 'SK', 'AB', 'BC', 'YT', 'NT', 'NU')
+                                   THEN 1 ELSE 0 END), 0) / CAST(COUNT(*) AS DECIMAL) can_rev_ratio
+         FROM review r
+                  JOIN business b
+                       ON r.business_id = b.business_id
+         GROUP BY r.user_id
+                  -- only select Canadian residents: users with > 60% of reviews recorded in Canada
+         HAVING COALESCE(SUM(CASE WHEN b.state IN
+                                       ('NL', 'PE', 'NS', 'NB', 'QC', 'ON', 'MB', 'SK', 'AB', 'BC', 'YT', 'NT', 'NU')
+                                      THEN 1 ELSE 0 END), 0) / CAST(COUNT(*) AS DECIMAL) > 0.6
+     ) AS can_users
+         INNER JOIN review r2
+                    ON r2.user_id = can_users.can_user_id
+         JOIN business b2
+              ON r2.business_id = b2.business_id
+WHERE b2.name = 'Mon Ami Gabi'
+  AND r2.date LIKE '2018%';
 
 
 -- Question 4: top 10 most common words in the reviews of the business “Chipotle Mexican Grill” might
 -- be helpful and interesting to the business.
 SELECT * FROM
-    ts_stat('SELECT to_tsvector(r.text) FROM review_cleaned AS r JOIN business b on r.business_id = b.business_id WHERE b.name=''Chipotle Mexican Grill''')
+    ts_stat('SELECT to_tsvector(r.text) FROM review AS r JOIN business b on r.business_id = b.business_id WHERE b.name=''Chipotle Mexican Grill''')
 ORDER BY nentry DESC
 LIMIT 10;
+
 
 -- Question 5: What’s the percentage of users, who reviewed ​“Mon Ami Gabi”,
 -- and also reviewed at least 10 other restaurants located in Ontario?
