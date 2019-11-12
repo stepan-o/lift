061949afad6620c60710cd2c7a2b6f913e37360d
Stepan Oskin <stepanoskin007@gmail.com>
2019-11-12 11:31:41 -0500

    scripts to setup MongoDB and PostgreSQL databases for Yelp dataset

--- src/yelp_db_setup_mongo.py
+++ src/yelp_db_setup_mongo.py
@@ -1,5 +1,8 @@
+# script to setup MongoDB database for Yelp dataset, ingest data, and create indices
+
 from db.mongo_db_setup import *
-from db.source_file_list import source_file_list
+from db.source_file_list import *
+import os
 
 
 # set up database
@@ -7,7 +10,12 @@ db_name = 'yelp'
 conn_string = 'mongodb://yelp:yelp@localhost:27017/{0}'.format(db_name)
 client, db = mongo_connect(conn_string, db_name)
 print("------ Ingesting data")
-ing_list = source_file_list()
+ing_list = get_source_file_list()
+# data_path = os.path.join('data', 'yelp_dataset')
+# review_path = os.path.join(data_path, 'review.json')
+# ingest_review = {'file_path': review_path, 'target_table': 'review'}
+# ing_list = [ingest_review]
+
 for data_source in ing_list:
     try:
         print("\n------ Inserting data into collection '{0}' from file '{1}'"
@@ -15,8 +23,14 @@ for data_source in ing_list:
         json_to_mongo(db, data_source['target_table'], data_source['file_path'])
 
     except pymongo.errors.OperationFailure as err:
-        # do whatever you need
         print("{0} Error when ingesting data into {1}:\n{2}".format('-' * 15, data_source['target_table'], err))
 
-client.close()
+idx_dict_list = get_idx_dict_list()
+print("\n------ Setting up indexes")
+for idx_dict in idx_dict_list:
+    mongo_index(db, idx_dict)
+print("------ All indexes have been created\n")
 
+print("------ Closing connection to database")
+
+client.close()
--- src/yelp_db_setup_postgres.py
+++ src/yelp_db_setup_postgres.py
@@ -1,7 +1,7 @@
 import os
 from glob import glob
 from db.postgres_db_setup import *
-from db.source_file_list import source_file_list
+from db.source_file_list import get_source_file_list
 
 conn_params = {'host': 'localhost',
                'port': '5432',
@@ -9,7 +9,7 @@ conn_params = {'host': 'localhost',
                'user': 'yelp',
                'password': 'yelp'}
 
-# read all SQL scripts from sql/DDL folder
+# read all SQL scripts for data definition from sql/DDL folder
 ddl_path = os.path.join('sql', 'DDL')
 ddl_file_list = glob(os.path.join(ddl_path, '*.sql'))
 ddl_query_list = list()
@@ -17,7 +17,7 @@ for ddl_file in ddl_file_list:
     with open(ddl_file, 'r') as f:
         ddl_query_list.append(f.read())
 
-# read all SQL scripts from sql/DDL folder
+# read all SQL scripts for data modification from sql/DML folder
 dml_path = os.path.join('sql', 'DML')
 dml_file_list = glob(os.path.join(dml_path, '*.sql'))
 dml_query_list = list()
@@ -25,7 +25,8 @@ for dml_file in dml_file_list:
     with open(dml_file, 'r') as f:
         dml_query_list.append(f.read())
 
-ing_list = source_file_list()
+# get the list of data sources to ingest into the database
+ing_list = get_source_file_list()
 
 # set up database
-db_setup(conn_params, ddl_queries=ddl_query_list, ingest_list=ing_list)
+db_setup(conn_params, ddl_queries=ddl_query_list, ingest_list=ing_list, dml_queries=dml_query_list)
