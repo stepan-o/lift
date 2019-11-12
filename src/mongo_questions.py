13c8f3cbb5c009ca82a121c2e438cebd98bfd3da
Stepan Oskin <stepanoskin007@gmail.com>
2019-11-12 11:31:21 -0500

    script with queries to answer Lift challenge questions using MongoDB

--- src/mongo_questions.py
+++ src/mongo_questions.py
@@ -4,6 +4,7 @@ from db.mongo_db_setup import *
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 50)
+pd.set_option('display.max_colwidth', 300)
 pd.set_option('display.width', 1000)
 
 db_name = 'yelp'
@@ -19,7 +20,7 @@ try:
     print("-- Querying collection {0}...".format(collection_name))
     pat = re.compile(r'Restaurants', re.I)
     results = collection.find(
-        {"city": "Toronto", "stars": {"$gt": 4.4}, "categories": {"$regex": "/Restaurants/"}},
+        {"city": "Toronto", "stars": {"$gt": 4.4}, "categories": pat},
         {"name": 1, "stars": 1, "review_count": 1, "_id": 0}
     ).sort([("review_count", -1), ("stars", -1)]).limit(10)
     results = pd.DataFrame(results)
@@ -50,76 +51,93 @@ except pymongo.errors.OperationFailure as err:
 
 # Question 3: How many Canadian residents reviewed the business “Mon Ami Gabi” in last 1 year?
 try:
-    join_key = 'business_id'
     print("\n------ Question 3, number of Canadian residents who reviewed Mon Ami Gabi in the last year:")
     ca_prov_list = ['NL', 'PE', 'NS', 'NB', 'QC', 'ON', 'MB', 'SK', 'AB', 'BC', 'YT', 'NT', 'NU']
-    ca_users = db['business'].aggregate([
-        {"$project": {"_id": 0,
-                      "b": "$$ROOT",
-                      "can_business":
-                          {"$cond": [
-                              {"$in": ["state", ca_prov_list]}, 1, 0
-                          ]}}},
+    ca_users_pipeline = [
+        {"$project": {
+            "_id": 0,
+            "b_name": "$name",
+            "b_state": "$state",
+            "b_business_id": "$business_id",
+            "b_ca_business": {
+                "$cond": [
+                    {
+                        "$in": ["$state", ca_prov_list]
+                    },
+                    1, 0
+                ]
+            }
+        }},
         {"$lookup": {
             "from": "review",
-            "localField": "b.business_id",
+            "localField": "b_business_id",
             "foreignField": "business_id",
             "as": "r"
         }},
-        {"$unwind": {"path": "$r", "preserveNullAndEmptyArrays": False}},
-        {"$group": {"_id": "$r.user_id", "ca_reviews": {"$sum": "$can_business"}, "total_reviews": {"$sum": 1}}},
-        {"$project": {
-            "_id": 0, "user_id": "$_id",
-            "ca_reviews": "$ca_reviews", "total_reviews": "$total_reviews",
-            "ca_ratio": {"$divide": ["$ca_reviews", "$total_reviews"]},
+        {"$unwind": {
+            "path": "$r",
+            "preserveNullAndEmptyArrays": False
         }},
-        {"$match": {"ca_ratio": {"$gt": 0.6}}},
-        {"$project": {"ca_users": {"$sum": 1}}}
-#         {"$match": {"b.name": "Mon Ami Gabi"}}
-    ], allowDiskUse=True)
-    # ca_reviews = db['review'].find({"business_id": {"$in": ca_businesses["business_id"].to_list()}})\
-    # ca_reviews = db['review'].count_documents({"business_id": {"$in": ca_bus_idx_list}})
-#    mon_ami_gabi = db['business'].find_one({"name": "Mon Ami Gabi"})
-#    mag_review_users = db['review'].find({"business_id": mon_ami_gabi["business_id"]}).distinct("user_id")
-    results = ca_users
-    results = pd.DataFrame(results)
-    print(results)
+        {"$project": {
+            "name": "$b_name",
+            "ca_business": "$b_ca_business",
+            "user_id": "$r.user_id"
+        }},
+        {"$group": {
+            "_id": "$user_id",
+            "ca_reviews": {
+                "$sum": "$ca_business"
+            },
+            "total_reviews": {
+                "$sum": 1
+            }
+        }},
+        {"$project": {
+            "_id": 0,
+            "user_id": "$_id",
+            "ca_ratio": {
+                "$divide": [
+                    {
+                        "$convert": {
+                            "input": "$ca_reviews",
+                            "to": "decimal"
+                        }
+                    },
+                    "$total_reviews"
+                ]
+            }
+        }},
+        {"$match": {
+            "ca_ratio": {
+                "$gt": 0.6
+            }
+        }}
+    ]
+#    explain = db.command('aggregate', 'business', pipeline=ca_users_pipeline, explain=True)
+#    print(explain['stages'])
+    t = time()
+    ca_users = db['business'].aggregate(ca_users_pipeline, allowDiskUse=True)
+    ca_users = pd.DataFrame(ca_users)
+    ca_users = ca_users['user_id'].to_list()
+    print("CA users: {0:,} users total".format(len(ca_users)))
+
+    mon_ami_gabi = db['business'].find_one({"name": "Mon Ami Gabi"})
+    pat = re.compile(r'2018', re.I)
+    mag_review_users = db['review'].find({
+        "business_id": mon_ami_gabi["business_id"],
+        "date": pat
+    }).distinct("user_id")
+    print("Users who reviewed Mon Ami Gabi in 2018: {0:,} users total".format(len(mag_review_users)))
+
+    intersection = [user for user in mag_review_users if user in ca_users]
+    elapsed = time() - t
+    print("-- Query completed, took {0:,.2f} seconds ({1:,.2f} minutes)."
+          .format(elapsed, elapsed / 60))
+    print("{0:,} users from Canada have reviewed Mon Ami Gabi in the past year".format(len(intersection)))
 
 except pymongo.errors.OperationFailure as err:
     # do whatever you need
     print("{0} Error when querying database:\n{1}".format('-' * 15, err))
-
-
-"""
-# Question 3: How many Canadian residents reviewed the business “Mon Ami Gabi” in last 1 year?
-try:
-    collection_name1 = 'business'
-    collection_name2 = 'review'
-    join_key = 'business_id'
-    collection1 = db[collection_name1]
-    print("\n------ Question 3, number of Canadian residents who reviewed Mon Ami Gabi in the last year:")
-    print("-- Querying collections {0} and {1}...".format(collection_name1, collection_name2))
-#    pat = re.compile(r'Mallo', re.I)
-    results = collection1.aggregate([
-        {"$project": {"_id": 0, "b": "$$ROOT"}},
-        {"$lookup":
-            {
-                "from": collection_name2,
-                "localField": "b.{0}".format(join_key),
-                "foreignField": join_key,
-                "as": "r"
-            }},
-        {"$unwind": {"path": "$r", "preserveNullAndEmptyArrays": False}},
-        {"$match": {"b.name": "Mon Ami Gabi"}},
-        {"$count": "selected_reviews"}
-    ])
-    results = pd.DataFrame(results)
-    print(results)
-
-except pymongo.errors.OperationFailure as err:
-    # do whatever you need
-    print("{0} Error when querying database:\n{1}".format('-' * 15, err))
-"""
 
 
 client.close()
