a2145a6a5230e5d4dffb59ba3c2b3478627a7294
Stepan Oskin <stepanoskin007@gmail.com>
2019-11-12 11:24:07 -0500

    DML: referential integrity constraints for Yelp dataset

--- sql/DML/DML_ref_integrity_constraints.sql
+++ sql/DML/DML_ref_integrity_constraints.sql
@@ -5,3 +5,15 @@ alter table review
 alter table review
     add constraint review_business_business_id_fk
         foreign key (business_id) references business;
+
+alter table checkin
+	add constraint checkin_business_business_id_fk
+		foreign key (business_id) references business;
+
+alter table tip
+	add constraint tip_yelp_user_user_id_fk
+		foreign key (user_id) references yelp_user;
+
+alter table tip
+	add constraint tip_business_business_id_fk
+		foreign key (business_id) references business;
